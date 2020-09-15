ff605a771e1df60867487700b2059f038253c20a
Benjamin De Bruyne <b.debruyne@gambit-finance.com>
2020-09-15 15:34:15 +0200

    more stats

--- lppls/signal/confidence.py
+++ lppls/signal/confidence.py
@@ -35,13 +35,13 @@ def LPPL_confidence(log_price, time_windows):
                 LPPL_confidences.append(0)
     conf = np.mean(LPPL_confidences)*np.sign(np.median(total_returns))
     res = {'conf': conf,
-     'tc_avg':np.mean(tcs), 'tc_std':np.std(tcs),
-     'omega_avg':np.mean(omegas), 'omega_std':np.std(omegas),
-     'beta_avg':np.mean(betas), 'beta_std':np.std(betas),
-     'A_avg':np.mean(As), 'A_std':np.std(As),
-     'B_avg':np.mean(Bs), 'B_std':np.std(Bs),
-     'C1_avg':np.mean(C1s), 'C1_std':np.std(C1s),
-     'C2_avg':np.mean(C2s), 'C2_std':np.std(C2s),
+     'tc_avg':np.mean(tcs), 'tc_std':np.std(tcs), 'tc_min':np.min(tcs), 'tc_max':np.max(tcs), 'tc_median':np.median(tcs), 'tc_q1':np.percentile(tcs,25), 'tc_q3':np.percentile(tcs,75),
+     'omega_avg':np.mean(omegas), 'omega_std':np.std(omegas), 'omega_min':np.min(omegas), 'omega_max':np.max(omegas), 'omega_median':np.median(omegas), 'omega_q1':np.percentile(omegas,25), 'omega_q3':np.percentile(omegas,75),
+     'beta_avg':np.mean(betas), 'beta_std':np.std(betas),'beta_min':np.min(betas), 'beta_max':np.max(betas), 'beta_median':np.median(betas), 'beta_q1':np.percentile(betas,25), 'beta_q3':np.percentile(betas,75),
+     'A_avg':np.mean(As), 'A_std':np.std(As), 'A_min':np.min(As), 'A_max':np.max(As), 'A_median':np.median(As), 'A_q1':np.percentile(As,25), 'A_q3':np.percentile(As,75),
+     'B_avg':np.mean(Bs), 'B_std':np.std(Bs), 'tc_min':np.min(Bs), 'tc_max':np.max(Bs), 'tc_median':np.median(Bs), 'B_q1':np.percentile(Bs,25), 'B_q3':np.percentile(Bs,75),
+     'C1_avg':np.mean(C1s), 'C1_std':np.std(C1s),'C1_min':np.min(C1s), 'C1_max':np.max(C1s), 'C1_median':np.median(C1s), 'C1_q1':np.percentile(C1s,25), 'C1_q3':np.percentile(C1s,75),
+     'C2_avg':np.mean(C2s), 'C2_std':np.std(C2s),'C2_min':np.min(C2s), 'C2_max':np.max(C2s), 'C2_median':np.median(C2s), 'C2_q1':np.percentile(C2s,25), 'C2_q3':np.percentile(C2s,75)
     }
     return res
 
