4afd06691956497e4330915544e286d0d901cf71
Benjamin De Bruyne <b.debruyne@gambit-finance.com>
2020-09-21 10:54:17 +0200

    change conditions

--- lppls/signal/conditions_satisfied.py
+++ lppls/signal/conditions_satisfied.py
@@ -6,11 +6,11 @@ def conditions_satisfied(beta, omega, tc, A, B, C1, C2, dt):
     #c3 = 0.95*dt < tc < 1.11*dt
     #print('tc in interval: ' + str(c3))
     C = np.sqrt(C1**2+C2**2) 
-    c3 = abs(C) < 1
+    #c3 = abs(C) < 1
     print('Amplitude of log-oscillations: ' + str(c3))
-    #c4 = 2.5 < np.abs(omega)/(2*np.pi)*np.log(abs(tc/(tc-dt)))
+    c4 = 2.5 < np.abs(omega)/2*np.log(abs(tc/(tc-dt)))
     #print('number oscillation: ' + str(c4))
-    c5 = 0.2 < beta * abs(B) / (omega * abs(C))
+    c5 = 0 < beta * abs(B) / (omega * abs(C))
     print('damping: ' + str(c5) )
     # return c1 and c2 and c3 and c4 and c5 
-    return c1 and c2 and c3 and c5 
+    return c1 and c2 and c4 and c5 
