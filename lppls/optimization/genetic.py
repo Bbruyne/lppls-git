82251cec2a79a0b223d153d9fae68bf974ecf5d8
Benjamin De Bruyne <b.debruyne@gambit-finance.com>
2020-09-23 09:50:29 +0200

    better genetic

--- lppls/optimization/genetic.py
+++ lppls/optimization/genetic.py
@@ -11,24 +11,29 @@ def fit_genetic(log_price):
 ]
     pop_num = 1000
     elite_num = int(pop_num*0.15)
-    cross_num = int(pop_num*0.5)
-    mut_num = int(pop_num*0.3)
+    cross_num = int(pop_num*0.20)
+    mut_num = int(pop_num*0.20)
     new_num = pop_num - elite_num - mut_num
-    gen_num = 10
-    
+    gen_num = 100
+    avg_elite_value = np.inf
     for gen in range(gen_num):
         if gen == 0:
             pop = _new(new_num=pop_num,init_limits=init_limits)
         else:
-            pop = elite
-            pop.extend(_crossover(elite=elite,cross_num=cross_num))
-            pop.extend(_mutation(elite=elite,mut_num=mut_num,init_limits=init_limits))
+            pop = elite.copy()
+            pop.extend(_crossover(elite=elite,elite_value=elite_value,cross_num=cross_num))
+            pop.extend(_mutation(elite=elite,elite_value=elite_value,mut_num=mut_num,init_limits=init_limits))
             pop.extend(_new(new_num=new_num,init_limits=init_limits))
 
         pop_eval = {func(x=chro, log_price=log_price):chro for chro in pop}
 
         elite_value = sorted(pop_eval)[:elite_num]
         elite = [pop_eval[value] for value in elite_value]
+        print('Residual :' + str(elite_value[0]))
+        if (avg_elite_value - np.mean(elite_value)) < 0.0001:
+            break
+        else:
+            avg_elite_value = np.mean(elite_value)
 
     tc, beta, omega = wrapper_scaling(pop_eval[elite_value[0]],log_price)
     normed_residual = func(pop_eval[elite_value[0]],log_price)/len(log_price)
@@ -38,18 +43,18 @@ def fit_genetic(log_price):
     return sol
 
 
-def _crossover(elite,cross_num):
+def _crossover(elite,elite_value,cross_num):
     pop_temp = []
     for _ in range(cross_num):
-        chro = random.choices(elite)[0]
-        chro_mate = random.choices(elite)[0]
+        chro = random.choices(elite,weights=1/np.array(elite_value))[0]
+        chro_mate = random.choices(elite,weights=1/np.array(elite_value))[0]
         pop_temp.append([chro[0], chro[1], chro_mate[2]])
     return pop_temp
 
-def _mutation(elite,mut_num,init_limits):
+def _mutation(elite,elite_value,mut_num,init_limits):
     pop_temp = []
     for _ in range(mut_num):
-        chro = random.choices(elite)[0]
+        chro = random.choices(elite,weights=1/np.array(elite_value))[0]
         gene = random.randrange(len(chro))
         chro_mutated = chro.copy()
         chro_mutated[gene] = random.uniform(init_limits[gene][0], init_limits[gene][1]) 
