d71ab2fb20ef55ca2bbfaae5c16bb70199e6f2ea
Benjamin De Bruyne <b.debruyne@gambit-finance.com>
2020-09-23 11:41:53 +0200

    quiet cmaes

--- lppls/optimization/cmaes.py
+++ lppls/optimization/cmaes.py
@@ -21,7 +21,7 @@ def fit_cma7(log_price):
     seed = np.array([tc_0, beta_0, omega_0, A, B, C1, C2])
     
         
-    opti_sol = cma.fmin(lambda x: func7(x[:3],x[3],x[4],x[5],x[6],log_price),sigma0=2,x0=seed)
+    opti_sol = cma.fmin(lambda x: func7(x[:3],x[3],x[4],x[5],x[6],log_price),sigma0=2,x0=seed,verbose=-1)
     tc, beta, omega = wrapper_scaling(opti_sol[0][:3],log_price)
     normed_residual = func7(opti_sol[0][:3],opti_sol[0][3],opti_sol[0][4],opti_sol[0][5],opti_sol[0][6],log_price)/len(log_price)
     sol = {'tc':tc, 'beta':beta, 'omega':omega, 'A':opti_sol[0][3], 'B':opti_sol[0][4], 'C1':opti_sol[0][5], 'C2':opti_sol[0][6], 'Res':normed_residual}
